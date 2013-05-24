6a05fb9931379dd177b51101c36a8580071a4a03
Chris Moss <chris@publicfunction.co.uk>
2013-05-24 16:30:20 +0100

    Update to views to use slug at all times, templates updated to reflect this.
    Signed-off-by: Chris Moss <chris@publicfunction.co.uk>

--- categories/views.py
+++ categories/views.py
@@ -5,41 +5,32 @@ from models import Category
 from products.models import Product
 
 def get_categories(request):
-    print "GET CAT CALLED"
+    print "GET CAT VIEW CALLED"
     children = Category.objects.filter(parent__isnull=False, published=True)    
     return render_to_response('category/list.html', 
                               {'list' : children},
                               RequestContext(request))
 
 def view_category(request, category):
-    print "VIEW CALLED"
-    if "-" in category:
-        cat = category.split('-')
-        cat = '%s %s' % (cat[0], cat[1])
-        print cat
-    else:
-        cat = category
-
-    list = Category.objects.filter(parent__name__icontains=cat)
+    print "VIEW CATEGORY VIEW CALLED"
+    list = Category.objects.filter(parent__slug__icontains=category)
     if not list:
-        list = Category.objects.filter(name__icontains=cat)
+        list = Category.objects.filter(slug=category)
     
     return render_to_response('category/list.html', 
-                              { 'list' : list},
+                              { 'list' : list,
+                                'category' : category},
                               RequestContext(request))
 
 def view_product_list(request, category, sub_category):
-    print "VIEW PRODUCT LIST"
-    if "-" in sub_category:
-        cat = sub_category.split('-')
-        cat = '%s %s' % (cat[0], cat[1])
-    else:
-        cat = sub_category
+    print "VIEW PRODUCT LIST VIEW CALLED"
+    category = "%s - %s" % (category, sub_category)
     
-    products = Product.objects.filter(category__name__icontains=cat)
+    products = Product.objects.filter(category__slug=sub_category)
     
     return render_to_response('product/list.html', 
-                              { 'list' : products },
+                              { 'list' : products, 
+                                'category' : category},
                               RequestContext(request))
 
 
